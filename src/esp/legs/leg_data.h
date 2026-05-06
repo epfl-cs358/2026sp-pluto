d01be1d4dfcdb77d7b3c236f37f26e2f3e00fdfb
R533-Code <raphaeldibnehmepc@gmail.com>
2026-05-06 10:46:02 +0200

    Added leg_data to centralize info

---
+++ src/esp/legs/leg_data.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <cstdint>
+
+namespace pluto
+{
+  struct LegData
+  {
+    uint16_t raw_min;
+    uint16_t raw_max;
+    uint16_t raw_start;
+    uint32_t angle_min_md;
+    uint32_t angle_max_md;
+  };
+
+  // TODO: compute angles
+
+  static constexpr LegData LEG_DATA_TOP  = {250, 500, 315, 0, 1};
+  static constexpr LegData LEG_DATA_COXA = LEG_DATA_TOP;
+
+  static constexpr LegData LEG_DATA_MIDDLE = {100, 150, 240, 0, 1};
+  static constexpr LegData LEG_DATA_FEMUR  = LEG_DATA_MIDDLE;
+
+  static constexpr LegData LEG_DATA_BOTTOM = {340, 400, 465, 0, 1};
+  static constexpr LegData LEG_DATA_TIBIA  = LEG_DATA_BOTTOM;
+} // namespace pluto
+
+#define PLUTO_EXPAND_LEG_DATA(leg_data)                                          \
+  leg_data.raw_min, leg_data.raw_max, leg_data.raw_start, leg_data.angle_min_md, \
+      leg_data.angle_max_md
