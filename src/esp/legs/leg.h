1c63537c778e8a469a2ac20f2ba47322f6c29e5f
R533-Code <raphaeldibnehmepc@gmail.com>
2026-05-06 11:48:42 +0200

    Added new APIs to Leg

--- src/esp/legs/leg.h
+++ src/esp/legs/leg.h
@@ -21,7 +21,36 @@ namespace pluto
     /// Handles vertical rotation of the knee to manage reach and ground contact.
     BOTTOM = 2,
     TIBIA  = BOTTOM,
+
+    /// @brief Count of enums. Do not use!
+    _count_LegJointType,
   };
+
+  /// @brief Returns the next joint type.
+  /// After BOTTOM, this returns TOP.
+  /// @param joint The current joint type
+  /// @return The next joint type
+  constexpr LegJointType next_joint_type(LegJointType joint) noexcept
+  {
+    return (LegJointType)(((uint8_t)joint + 1)
+                          % (uint8_t)LegJointType::_count_LegJointType);
+  }
+
+  /// @brief Converts a LegJointType to a string.
+  /// @param joint The joint type
+  /// @return String
+  constexpr const char* str_joint_type(LegJointType joint) noexcept
+  {
+    switch (joint)
+    {
+    case LegJointType::TOP:
+      return "COXA (TOP)";
+    case LegJointType::MIDDLE:
+      return "FEMUR (MIDDLE)";
+    case LegJointType::BOTTOM:
+      return "TIBIA (BOTTOM)";
+    }
+  }
 
   /// @brief Identifiers for the physical position of each leg on the chassis.
   enum class LegSide : uint8_t
@@ -53,5 +82,62 @@ namespace pluto
     Leg(const Leg&) noexcept            = default;
     Leg& operator=(Leg&&) noexcept      = default;
     Leg& operator=(const Leg&) noexcept = default;
+
+    /// @brief Indexes into the joints
+    /// @param type The LegJointType
+    /// @return View over a LegJoint
+    LegJointView operator[](LegJointType type) noexcept
+    {
+      switch (type)
+      {
+      case LegJointType::COXA:
+        return LegJointView(top);
+      case LegJointType::FEMUR:
+        return LegJointView(middle);
+      case LegJointType::TIBIA:
+        return LegJointView(bottom);
+      }
+    }
+
+    /// @brief For each joint, call a function.
+    /// The functor should usually be a templated lambda.
+    /// @tparam T The lambda
+    /// @param functor The functor
+    template<typename T>
+    void for_each_joint(T&& functor) noexcept
+    {
+      functor(top);
+      functor(middle);
+      functor(bottom);
+    }
+
+    /// @brief Moves all joints in the leg to their starting positions.
+    void write_starting() noexcept
+    {
+      top.write_starting();
+      middle.write_starting();
+      bottom.write_starting();
+    }
+
+    /// @brief Sets all three joint angles simultaneously.
+    /// @param coxa_md The coxa angle in millidegrees
+    /// @param femur_md The femur angle in millidegrees
+    /// @param tibia_md The tibia angle in millidegrees
+    void write_angles(int32_t coxa_md, int32_t femur_md, int32_t tibia_md) noexcept
+    {
+      top.write_angle(coxa_md);
+      middle.write_angle(femur_md);
+      bottom.write_angle(tibia_md);
+    }
+    /// @brief Sets all three joint raw values simultaneously
+    /// @param coxa_raw The coxa raw PWM
+    /// @param femur_raw The femor raw PWM
+    /// @param tibia_raw The tibia raw PWM
+    void write_raws(int16_t coxa_raw, int16_t femur_raw, int16_t tibia_raw) noexcept
+    {
+      top.write_raw(coxa_raw);
+      middle.write_raw(femur_raw);
+      bottom.write_raw(tibia_raw);
+    }
   };
 } // namespace pluto
